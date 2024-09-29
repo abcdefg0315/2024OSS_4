af56d4dbc94001618cbff6d5619e11bf2c3703c8
SSJ <ssj49552187@gmail.com>
2024-09-29 18:10:07 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {Hello,Id} from './Hello.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello/>
+        <Id/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import './Hello.css';
+
+function Hello(props){
+    return (
+        <h1>Hello World!!!</h1>
+    );
+}
+
+function Id(props){
+    return(<h2 className="id-color">22300383</h2>);
+}
+
+export {Hello,Id};
